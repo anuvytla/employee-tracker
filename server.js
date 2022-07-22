35e3496cf24dba42a9dee82c5010bba6215eff08
Lakshmi Anusha Vytla <reachinganusha@gmail.com>
2022-07-21 22:28:33 -0700

    Add inquirer prompts for take user input with placeholder functions

--- server.js
+++ server.js
@@ -1,10 +1,93 @@
 const mysql = require('mysql2');
+const inquirer = require('inquirer');
 
 var connection = mysql.createConnection({
     host: 'localhost',
-    Port : 3001,
     user : 'root',
     password : 'password',
     database : 'employee_db'
 });
 
+connection.connect(err => {
+    if (err) throw err;
+    showManagementOptions();
+});
+
+const showManagementOptions = async() => {
+    try {
+        let answer = await inquirer.prompt({
+            name : 'action',
+            type: 'list',
+            message : 'what would you like to do?',
+            choices : [
+                'View Employees',
+                'View Departments',
+                'View Roles',
+                'Add Employees',
+                'Add Departments',
+                'Add Roles',
+                'Update Employee Role'
+            ]
+        });
+        switch(answer.action) {
+            case 'View Employees':
+                viewAllEmployees();
+                break;
+    
+            case 'View Departments':
+                viewAllDepartment();
+                break;
+    
+            case 'View Roles':
+                viewAllRoles();
+                break;
+    
+            case 'Add Employees':
+                addEmployees();
+                break;
+    
+            case 'Add Departments':
+                addDepartments();
+                break;
+    
+            case 'Add Roles':
+                addRoles();
+                break;
+    
+            case 'Update Employee Role':
+                updateEmployee();
+                break;
+        }
+    }
+     catch (err) {
+        console.log(err);
+    };
+}
+
+const viewAllEmployees = async() => {
+
+}
+
+const viewAllDepartment = async() => {
+
+}
+
+const viewAllRoles = async() => {
+
+}
+
+const addEmployees = async() => {
+
+}
+
+const addDepartments = async() => {
+
+}
+
+const addRoles = async() => {
+
+}
+
+const updateEmployee = async() => {
+
+}
